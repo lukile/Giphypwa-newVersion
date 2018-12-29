d6b8eb46da989a8be12383333080243007b0fcc3
lukile <lucile.1988.ls@gmail.com>
2018-12-29 15:48:19 +0100

    Made first page witch api call to giphy and display random gif

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <nav class="navbar has-shadow head-bar">
+      <div class="container">
+        <div class="navbar-brand"><img class="logo-header" src="@/assets/giphy-logo.png" height="200px"/>
+          <div class="navbar-burger burger" data-target="navMenu"><span></span><span></span><span></span></div>
+        </div>
+        <div class="navbar-menu" id="navMenu">
+          <p class="title-header">GIPHY PWA</p>
+        </div>
+      </div>
+    </nav>
+
+    <router-view></router-view>
+
+
+
+    <footer>
+      <div id="footer">
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+    }
+</script>
+
+<style>
+  @import '../node_modules/bulma/css/bulma.css';
+
+  body {
+    margin: 0;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+
+  main {
+    text-align: center;
+    margin-top: 40px;
+  }
+
+  header {
+    margin: 0;
+    height: 56px;
+    padding: 0 16px 0 24px;
+    background-color: #35495E;
+    color: #ffffff;
+  }
+
+  .head-bar {
+    background-color: #0e0202;
+    height: 70px;
+    margin-bottom: 30px;
+  }
+
+  .logo-header {
+    width: 70px;
+  }
+
+  .title-header {
+    margin-top: 11px;
+    font-size: 30px;
+    color: white;
+    font-weight: bold;
+  }
+
+
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="box cta random-title">
+      <div class="columns is-mobile is-centered">
+        <div id="app" class="random-text">
+        </div>
+      </div>
+    </div>
+    <section class="container random-body-section">
+      <div class="level-item">
+        <div class="columns is-multiline is-centered cards-container" id="sectioncontainer">
+          <div class="column is-narrow">
+            <article class="message is-link">
+              <div class="message-header">
+                <img :src="randomGif" height="200px"/>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+
+
+
+
+    </section>
+    <div class="columns is-mobile is-centered">
+      <div class="column is-half is-narrow"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        search: "",
+        title: "Random gif",
+        randomGif: ''
+      }
+    },
+
+    created() {
+      axios.get("http://api.giphy.com/v1/gifs/random?&api_key=XrA7LAMCijdvX3iUk9cu7uO1uxB83IWO&limit=1")
+        .then(response => {
+          //JSON parsed
+          this.randomGif = response.data.data.images.original.url;
+        })
+        .catch(error => {
+          console.log("Error : " + error)
+        })
+    },
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #35495E;
+  }
+</style>
