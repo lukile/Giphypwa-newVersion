39d95178e77f9b57dacad888c94e0a1e8130b05c
Florian <Florian@software-attitude.com>
2019-01-09 18:15:28 +0100

    add sw

---
+++ src/service-worker.js
@@ -0,0 +1,42 @@
+var cacheName = 'giphypwa';
+var filesToCache = [
+  '/static/img/icons/android-chrome-192x192.png',
+  '/static/img/icons/android-chrome-512x512.png',
+  '/static/img/icons/apple-touch-icon-60x60.png',
+  '/static/img/icons/apple-touch-icon-76x76.png',
+  '/static/img/icons/safari-pinned-tab.svg',
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
