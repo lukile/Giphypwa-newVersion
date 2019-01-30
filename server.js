25dbe4c650e8b40e2b39656845a14bf8bf55e47c
andrea <laureenmar.15@gmail.com>
2019-01-30 21:51:26 +0100

    add env to sslRedirect heroku

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use (function (req, res, next) {
     }
 });*/
 
-app.use(sslRedirect());
+app.use(sslRedirect(['production'], 301));
 
 //app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
 
