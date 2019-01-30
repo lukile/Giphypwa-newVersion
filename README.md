cf81061eca6711443f1d2dba6ab76ee74ee5482f
andrea <laureenmar.15@gmail.com>
2019-01-30 21:21:50 +0100

    add cors

--- README.md
+++ README.md
@@ -33,6 +33,12 @@ npm run e2e
 
 # run all tests
 npm test
+
+# launch online (compile and launch)
+npm run build
+npm run start
+
 ```
 
 For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+const cors = require('cors');
 
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // tous les requêtes du site je les accepte -- fails de sécurité
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+});*/
+app.use(cors());
 
 app.use (function (req, res, next) {
     if (req.secure) {
