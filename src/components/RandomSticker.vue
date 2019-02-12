a74e618070d0a3356c369f7b783e84ba1831c086
lukile <lucile.1988.ls@gmail.com>
2019-02-12 21:42:22 +0100

    Re added random sticker

---
+++ src/components/RandomSticker.vue
@@ -0,0 +1,48 @@
+<template lang="html">
+  <div>
+      <div class="box cta random-title">
+        <p class="title" align="center">{{ title }}</p>
+      </div>
+
+      <section>
+        <div class="sc">
+          <div class="columns is-multiline is-half-mobile is-half-tablet is-half-desktop is-half-widescreen is-half-fullhd">
+            <div class="column is-half is-offset-4">
+              <img class="se" :src="randomSticker" height="200px" alt="randomSticker">
+            </div>
+          </div>
+        </div>
+      </section>
+  </div>
+</template>
+
+<script lang="js">
+  import axios from 'axios';
+
+  export default  {
+    data() {
+      return {
+        search: "",
+        title: "Random Sticker",
+        randomSticker: ''
+      }
+    },
+    created() {
+      axios.get("http://api.giphy.com/v1/stickers/random?&api_key=XrA7LAMCijdvX3iUk9cu7uO1uxB83IWO")
+        .then(response => {
+          this.randomSticker = response.data.data.images.original.url;
+        })
+    }
+}
+</script>
+
+<style scoped>
+  .random-title {
+    background-color: #0e0202;
+    height: 70px;
+  }
+
+  .title {
+    color: white;
+  }
+</style>
