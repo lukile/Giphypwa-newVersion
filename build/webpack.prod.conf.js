439cd63d44d24d05c3bc5388594a63fc4da3bcb9
Florian <florian.mazuy@gmail.com>
2019-02-18 21:18:49 +0100

    custom sw

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -28,8 +28,8 @@ const webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? '#source-map' : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    filename: utils.assetsPath('js/[name].js'),
+    chunkFilename: utils.assetsPath('js/[id].js')
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
